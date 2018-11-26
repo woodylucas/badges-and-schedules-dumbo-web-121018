42262a611efc4afc211b5e52dad0448be6f1853c
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 07:08:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,12 +12,5 @@ def batch_badge_creator(speakers)
   badge_messages
 end 
 
-def assign_rooms(speakers)
-  room_number = 1 
-  room_messages = []
-  speakers.each do |speaker|
-    room_messages << "Hello, #{speaker}! You'll be assigned to room #{room_number}"
-    room_number += 1 
-  end 
-  room_messages 
-end 
+
+def 
