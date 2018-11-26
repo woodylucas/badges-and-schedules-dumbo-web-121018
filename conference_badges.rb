b69c5bc11f8319c831f171cfc3729f48864fdeea
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 06:59:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(speaker)
+def batch_badge_creator(speakers)
   badge_messages = []
   badge_messages.each do |speaker|
     message = badge_maker
