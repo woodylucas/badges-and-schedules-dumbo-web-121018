6f4cf523b5f759c3b7c6e45f9e116965ce83d72b
Woody Lucas <wlucas0311@gmail.com>
2018-11-26 07:19:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,13 +23,3 @@ end
 room_messages
 end 
 
-def printer(speakers) 
-  badge_messages = batch_badge_creator(speakers)
-  batch_messages.each do |message|
-    puts message 
-  end 
-  room_messages = assign_rooms(speakers)
-  room_messages.each do |message|
-    puts message 
-  end 
-end 
